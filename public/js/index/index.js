089ec272d6f17ca6816b3ebd6d89015dad9f182a
jaywcjlove <398188662@qq.com>
2016-06-01 22:19:23 +0800

    添加测试文件

--- public/js/index/index.js
+++ public/js/index/index.js
@@ -3,6 +3,8 @@ require('../../styl/index.styl');
 
 var a = require('a')
 var b = require('b')
+var c = require('b')
 
-console.log("tes",a(10,1))
-console.log("tes",b(10,1))
+console.log("tes1:",a(10,1))
+console.log("tes2:",b(10,1))
+console.log("tes3:",c(10,1))
---
+++ public/js/sorting/a/a.js
@@ -0,0 +1,6 @@
+
+var a = require('a')
+var b = require('b')
+
+console.log("tes",a(10,1))
+console.log("tes",b(10,1))
